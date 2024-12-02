4ae5a92de26ebdd80e8e4065233ac92a98b6b95d
Ameen Adam <ameen2bman@gmail.com>
2024-12-02 18:16:34 +0200

    testing commit from VS code

--- GUI_TESTS.py
+++ GUI_TESTS.py
@@ -1,28 +1,50 @@
 from tkinter import *
 import time 
-
-win = Tk()
-win.geometry("450x450")
-win.title("YOOOOOOOOOOOO")
-
-img = PhotoImage(file="ICON.PNG")
-
-win.iconphoto(True,img)
-win.config(background="GREY") 
+def play():
+        
+    #================================= IMAGES ========================
 
 
 
-
-
-win.mainloop()
-
+    img = PhotoImage(file="ICON.PNG")
 
 
 
+    #================================= WINDOWS ========================
+
+    win = Tk()
+    win.geometry("450x450")
+    win.title("YOOOOOOOOOOOO")
+    win.iconphoto(True,img)
+    win.config(background="GREY") 
+
+
+
+    #================================= LABELS ========================
+
+    f = "HELLO WORLD"
+    label1 = Label(win , text = "HELLO WORLD" ,font=('',40))
 
 
 
 
+
+
+
+
+    #================================= START ========================
+
+    label1.place(width=450 , height=450 , x=0 , y=0)
+    win.mainloop()
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    play()
 
 
 
