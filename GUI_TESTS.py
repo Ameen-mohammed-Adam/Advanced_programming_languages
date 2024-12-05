b27e1fefc8c3aca112ea80a4927f5e4df290aa91
Ameen Adam <ameen2bman@gmail.com>
2024-12-05 19:25:16 +0200

    added the core of the main window

--- GUI_TESTS.py
+++ GUI_TESTS.py
@@ -1,86 +1,28 @@
-from tkinter import *
-import time 
-def play():
-        
-    #================================= IMAGES ========================
+from imported import *
 
 
 
-    img = PhotoImage(file="ICON.PNG")
+main_window = Tk()
+main_window.title("AIRPORT PARKING SYSTEM")
+img = PhotoImage(file="ICON.png")
+main_window.iconphoto(TRUE , img)
+main_window.resizable(False,False)
+main_window.geometry("450x450+400+150")
+main_window.config(background="black")
+
+header_label = Label(main_window , text="Welcome To OUR\nAIRPORT PARKING SYSTEM" , font=('', 15 , 'bold') ,
+                      foreground="white" , width=37 , height=6 , background="black" , pady=40)
+New_Account = Button(main_window , text="NEW USER" , background="#007bff" , foreground="WHITE" )
+Old_Account = Button(main_window , text="OLD USER" , background="#007bff")
+Employee = Button(main_window , text="Employees Only", background="#007bff")
+close = Button(main_window , text="EXIT" , width=10 , height=1 , background="CYAN" , command=exit)
 
 
-
-    #================================= WINDOWS ========================
-
-    win = Tk()
-    win.geometry("450x450")
-    win.title("YOOOOOOOOOOOO")
-    win.iconphoto(True,img)
-    win.config(background="GREY") 
-
-
-
-    #================================= LABELS ========================
-
-    f = "HELLO WORLD"
-    label1 = Label(win , text = "HELLO WORLD" ,font=('',40))
-
-
-
-
-
-
-
-
-    #================================= START ========================
-
-    label1.place(width=450 , height=450 , x=0 , y=0)
-    win.mainloop()
-
-    
-
-
-
-
+header_label.place(x = 0 , y = 0)
+New_Account.place(x = 300 , y = 300)
+close.place(x = 40 , y = 400)
+Employee.place(x = 40 , y = 400)
+main_window.mainloop()
 
 if __name__ == "__main__":
-    play()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pass
---
+++ imported.py
@@ -0,0 +1,5 @@
+from tkinter import *
+import time 
+import random
+import math
+from PIL import Image, ImageTk
